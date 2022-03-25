9170862e54a205a6d93d5108b28fcf7f239d67d1
NaudR366 <naudgm@outlook.com>
2022-03-25 11:29:28 +0100

    new init (expo)

---
+++ App.js
@@ -0,0 +1,13 @@
+import Navigator from './nav/stacknav.js'
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs([
+  "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
+]);
+
+export default function App() {
+  return (
+    <Navigator />
+  );
+}
+
