9dd5dbc32fa3abd8a1c294c38b9cc8941bfec0c9
NaudR366 <naudgm@outlook.com>
2022-04-04 20:38:12 +0200

    public expo + bg added cards view

--- screens/Cards.js
+++ screens/Cards.js
@@ -1,5 +1,5 @@
 import { useRef } from 'react';
-import { Text, View, Image, ScrollView, Animated } from 'react-native';
+import { Text, View, Image, ScrollView, Animated, ImageBackground } from 'react-native';
 import { globalStyles } from "../styles/global";
 import BackButton from '../components/BackButton.js'
 
@@ -8,25 +8,27 @@ export default function Cards({ navigation }) {
     const slidesRef = useRef(null)
     return (
         <View style={globalStyles.container}>
-            <Image style={globalStyles.oasenLogoOptions} source={require('../assets/oasen.png')} />
-            <BackButton nav={navigation} navTo="OptionsView" />
+            <ImageBackground source={require('../assets/bg.png')} style={{ width: '100%', height: '100%' }}>
+                <Image style={globalStyles.oasenLogoOptions} source={require('../assets/oasen.png')} />
+                <BackButton nav={navigation} navTo="OptionsView" />
 
-            <View style={globalStyles.CardContainer}>
-                <ScrollView showsVerticalScrollIndicator={false} style={{ marginBottom: 250 }} horizontal scrollEnabled pagingEnabled scrollEventThrottle={16} showsHorizontalScrollIndicator={false} bounces={true}>
-                    <View style={{ marginLeft: 40, marginRight: 30, alignSelf: 'center' }} >
-                        <Image style={globalStyles.cardImage} source={require('../assets/Card-1.png')} />
-                        <Text style={globalStyles.CardText} >Grondwater oppompen</Text>
-                    </View>
-                    <View style={{ marginLeft: 60, marginRight: 20, alignSelf: 'center' }} >
-                        <Image style={globalStyles.cardImage} source={require('../assets/Card-2.png')} />
-                        <Text style={globalStyles.CardText} >Beluchting</Text>
-                    </View>
-                    <View style={{ marginLeft: 40, marginRight: 40, alignSelf: 'center' }} >
-                        <Image style={globalStyles.cardImage} source={require('../assets/Card-3.png')} />
-                        <Text style={globalStyles.CardText} >Zandfiltratie</Text>
-                    </View>
-                </ScrollView>
-            </View>
+                <View style={globalStyles.CardContainer}>
+                    <ScrollView showsVerticalScrollIndicator={false} style={{ marginBottom: 250 }} horizontal scrollEnabled pagingEnabled scrollEventThrottle={16} showsHorizontalScrollIndicator={false} bounces={true}>
+                        <View style={{ marginLeft: 40, marginRight: 30, alignSelf: 'center' }} >
+                            <Image style={globalStyles.cardImage} source={require('../assets/Card-1.png')} />
+                            <Text style={globalStyles.CardText} >Grondwater oppompen</Text>
+                        </View>
+                        <View style={{ marginLeft: 60, marginRight: 20, alignSelf: 'center' }} >
+                            <Image style={globalStyles.cardImage} source={require('../assets/Card-2.png')} />
+                            <Text style={globalStyles.CardText} >Beluchting</Text>
+                        </View>
+                        <View style={{ marginLeft: 40, marginRight: 40, alignSelf: 'center' }} >
+                            <Image style={globalStyles.cardImage} source={require('../assets/Card-3.png')} />
+                            <Text style={globalStyles.CardText} >Zandfiltratie</Text>
+                        </View>
+                    </ScrollView>
+                </View>
+            </ImageBackground>
 
         </View>
     );
